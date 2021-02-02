9ad922577cfb38f94abc8c35e933709cb1a8ef4e
Akshay VK <73469520+Akshay-VK@users.noreply.github.com>
2021-02-02 14:17:36 +0530

    basic tic tac toe layout done.. :D

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = process.env.port || 3000;
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port,()=>{
     console.log(`Server started st port ${port}`);
 })
