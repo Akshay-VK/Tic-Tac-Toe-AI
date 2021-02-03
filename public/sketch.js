93411df30c866cf3ce42d915895aa4a2ba58a0c1
Akshay VK <73469520+Akshay-VK@users.noreply.github.com>
2021-02-03 11:34:51 +0530

    Game code done.Still debugging as te winning logic is not working

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,65 @@
 var squares = [];
+var state = true;
+var winner = "";
+// true = O
+// false = X
+var options = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+for (var i = 0; i < 9; i++) {
+    var elm = document.getElementById("a" + (i + 1));
+    squares.push(elm);
+}
+console.log(squares);
 //INITIATION
-function initiate() {
-    for (var i = 0; i < 9; i++) {
-        var elm = document.getElementById("a" + (i + 1));
-        squares.push(elm);
+function initiateGame() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].innerHTML = "";
     }
 }
-initiate();
+//SETTING VALUE
+function setValue(id) {
+    if (squares[id].innerHTML == "") {
+        squares[id].innerHTML = state ? "O" : "X";
+        state = !state;
+    }
+    var winner = findWinner();
+    if (winner != "none") {
+        // for(var i = 0; i < squares.length; i++) {
+        //     squares[i].innerHTML = `Winner: ${winner}`;
+        // }
+        alert("Winner: " + winner);
+    }
+    alert("Returned winner: " + winner);
+}
+// 012
+// 345
+// 678
+function findWinner() {
+    winner = "";
+    for (var i = 0; i < options.length; i++) {
+        var a = options[i][0];
+        var b = options[i][1];
+        var c = options[i][2];
+        if (squares[a].innerHTML != "") {
+            if (squares[a].innerHTML == squares[b].innerHTML) {
+                if (squares[b].innerHTML == squares[c].innerHTML) {
+                    winner = squares[a].innerHTML;
+                }
+            }
+        }
+        else {
+            winner = "none";
+        }
+    }
+    console.log(winner);
+    return winner;
+}
+initiateGame();
--- public/sketch.ts
+++ public/sketch.ts
@@ -1,10 +1,88 @@
+export {};
 var squares:HTMLElement[] = [];
+var state: boolean = true;
+//var winner: string = "";
+var positions: Array<string> = [
+    "","","",
+    "","","",
+    "","","",    
+];
+// true = O
+// false = X
 
-//INITIATION
-function initiateGame(): void {
-    for(let i = 0;i<9;i++) {
+var options : Array<Array<Number>> = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]        
+];
+
+for(let i = 0;i<9;i++) {
         let elm:HTMLElement = document.getElementById(`a${i+1}`);
         squares.push(elm);
     }
     console.log(squares);
+
+//INITIATION
+function initiateGame() :void{
+    for(let i = 0;i<squares.length;i++) {
+        squares[i].innerHTML = "";
+    }
 }
+
+//SETTING VALUE
+function setValue(id: number): void {
+    if(squares[id].innerHTML == "") {
+        squares[id].innerHTML = state ? "O" : "X";
+        positions[id] = state ? "O" : "X";
+        
+        state = !state;
+    }
+    var winner: string = findWinner();
+    if(winner != "") {
+        // for(var i = 0; i < squares.length; i++) {
+        //     squares[i].innerHTML = `Winner: ${winner}`;
+        // }
+        alert(`Winner: ${winner}`);
+    }
+    alert(`Returned winner: ${winner}`);
+}
+
+// 012
+// 345
+// 678
+
+
+function findWinner(): string {
+    var winner: string = "";
+    for(var i = 0; i < options.length; i++) {
+        var a: any = options[i][0];
+        var b: any = options[i][1];
+        var c: any = options[i][2];
+        
+        if(squares[a].innerHTML != "") {
+            if(squares[a].innerHTML == squares[b].innerHTML){
+                if(squares[b].innerHTML == squares[c].innerHTML){
+                    winner = squares[a].innerHTML;
+                }else{            
+                    winner = "";
+                }
+            }else{            
+                winner = "";
+            }
+        }else{            
+            winner = "";
+        }
+        if(winner != ""){
+            break;
+        }
+    }
+    console.log(winner);
+    return winner;
+}
+
+initiateGame();
